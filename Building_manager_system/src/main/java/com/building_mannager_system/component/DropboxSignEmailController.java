631ca565ad68b162421df5311690ac8a3323a3ea
Thao <soulchancedream1101@gmail.com>
2025-03-07 08:37:37 +0700

    commit

---
+++ Building_manager_system/src/main/java/com/building_mannager_system/component/DropboxSignEmailController.java
@@ -0,0 +1,37 @@
+package com.building_mannager_system.controller;
+
+import com.hellosign.sdk.HelloSignException;
+import com.hellosign.sdk.resource.SignatureRequest;
+import com.building_mannager_system.component.DropboxSignEmailService;
+import com.building_mannager_system.utils.annotation.ApiMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/dropboxsign")
+public class DropboxSignEmailController {
+
+    @Autowired
+    private DropboxSignEmailService dropboxSignEmailService;
+
+    /**
+     * Endpoint gửi yêu cầu ký qua email sử dụng template của HelloSign.
+     * Ví dụ: POST /dropboxsign/send-email/{id}
+     *
+     * @param id ID của hợp đồng cần gửi yêu cầu ký.
+     * @return Thông báo kết quả gửi yêu cầu ký.
+     */
+    @PostMapping("/send-email/{id}")
+    @ApiMessage("Thành công")
+    public ResponseEntity<String> sendEmailSignatureRequest(@PathVariable(name = "id") int id) {
+        try {
+            SignatureRequest request = dropboxSignEmailService.sendEmailSignatureRequestUsingContract(id);
+            return ResponseEntity.ok("Yêu cầu ký đã được gửi qua email. Request ID: " + request.getId());
+        } catch (HelloSignException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Lỗi khi gửi yêu cầu ký qua email: " + e.getMessage());
+        }
+    }
+}
---
+++ Building_manager_system/src/main/java/com/building_mannager_system/dto/requestDto/ContractDto/ConfirmationRequestDto.java
@@ -0,0 +1,4 @@
+package com.building_mannager_system.dto.requestDto.ContractDto;
+
+public class ConfirmationRequestDto {
+}
